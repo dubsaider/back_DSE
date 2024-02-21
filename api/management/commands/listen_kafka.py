81222c562869814204d76e4cd2fde86ca0610bfe
Kostenkov Vladislav <kostenkov.va@dvfu.ru>
2024-02-21 20:33:06 +1000

    cut miliseconds

--- api/management/commands/listen_kafka.py
+++ api/management/commands/listen_kafka.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
 from kafka import KafkaConsumer
 import json
-import datetime
-from api.models import Incident, ZoneStats, CameraStats, Camera
+from datetime import datetime
+from api.models import Incident, ZoneStat, CameraStat, Camera
 
 
 class Command(BaseCommand):
@@ -18,58 +18,54 @@ class Command(BaseCommand):
         )
 
         aggregated_data = {}
+        last_second = None
 
         for message in consumer:
             msg = message.value
             
             if "event_name" in msg:
+                # TODO read incident
                 Incident.objects.create(
                     timestamp=datetime.now(),
                     camera=msg["device_info"]["IP"],
                     event=msg["event_name"],
                     incident=msg["msg"],
-                    link="your_link", # TODO save video 
+                    link="your_link", # TODO save video   
                 )
             elif "line_id" in msg:
                 msg_parts = msg.split()
                 date, time, _, _, camera_ip, _, line_id, _, zone_id, _, zone, _, in_count, _, out_count, _, direction = msg_parts
-                timestamp = f"{date}T{time}Z"
-                second_key = f"{timestamp[:19]}Z"
-                direction_key = f"{camera_ip}_{direction}"
-                if direction_key not in aggregated_data:
-                    aggregated_data[direction_key] = {
-                        'timestamp': second_key,
-                        'camera_ip': camera_ip,
-                        'direction': direction,
-                        'input_value':  1 if direction == 'in' else  0,
-                        'output_value':  0 if direction == 'in' else  1,
-                    }
-                else:
-                    aggregated_data[direction_key]['input_value'] +=  1 if direction == 'in' else  0
-                    aggregated_data[direction_key]['output_value'] +=  0 if direction == 'in' else  1
+                timestamp = f"{date}T{time[:8]}Z"
+                aggregation_key = f"{camera_ip}_{timestamp}"
 
-                if datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ') != second_key:
+                current_second = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+                if last_second is None or current_second > last_second:
                     for data in aggregated_data.values():
                         camera = Camera.objects.get(camera_ip=data['camera_ip'])
-                        CameraStats.objects.create(
-                            timestamp=datetime.datetime.strptime(data['timestamp'], "%Y-%m-%dT%H:%M:%SZ"),
+                        CameraStat.objects.create(
+                            timestamp=current_second,
                             camera=camera,
                             input_value=data['input_value'],
                             output_value=data['output_value'],
                         )
                     aggregated_data = {}
-            elif "msg_type" in msg:
-                ZoneStats.objects.create(
-                    timestamp=datetime.now(),
-                    camera=msg["device_info"]["IP"],
-                    location=msg["zone_id"],
-                    change=msg["msg"]["in"] - msg["msg"]["out"],
-                )
+                    last_second = current_second
+
+                if aggregation_key not in aggregated_data:
+                    aggregated_data[aggregation_key] = {
+                        'timestamp': timestamp,
+                        'camera_ip': camera_ip,
+                        'input_value':   1 if direction == 'in' else   0,
+                        'output_value':   0 if direction == 'in' else   1,
+                    }
+                else:
+                    aggregated_data[aggregation_key]['input_value'] +=   1 if direction == 'in' else   0
+                    aggregated_data[aggregation_key]['output_value'] +=   0 if direction == 'in' else   1
 
         for data in aggregated_data.values():
             camera = Camera.objects.get(camera_ip=data['camera_ip'])
-            CameraStats.objects.create(
-                timestamp=datetime.datetime.strptime(data['timestamp'], "%Y-%m-%dT%H:%M:%SZ"),
+            CameraStat.objects.create(
+                timestamp=current_second,
                 camera=camera,
                 input_value=data['input_value'],
                 output_value=data['output_value'],
