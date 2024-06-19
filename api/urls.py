35c93cdbbd457dea4e9b6b9959668f1da535e4a9
Kostenkov Vladislav <kostenkov.va@dvfu.ru>
2024-06-19 11:34:36 +1000

    added incident video

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,15 @@ from .views import (
     CameraStatViewSet,
     IncidentTypeViewSet,
 )
+import os
+from django.urls import path
+from .models import Incident, Camera  # Импорт моделей для работы с базой данных
+from django.http import HttpResponseNotFound  # Импорт для возврата ответа NotFound
+from pathlib import Path  # Импорт для работы с путями файловой системы
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseNotFound
+
+
 
 router = routers.DefaultRouter()
 router.register('incident', IncidentViewSet, basename='incident-viewset')
@@ -13,7 +22,26 @@ router.register('zone-stats', ZoneStatViewSet, basename='zone-stats-viewset')
 router.register('camera-stats', CameraStatViewSet, basename='camera-stats-viewset')
 router.register('incident-type', IncidentTypeViewSet, basename='incident-type-viewset')
 
+def get_incident_view(request=None, pk=None, filename='stream.m3u8'):
+    if not Incident.objects.filter(pk=pk).exists():
+        return HttpResponseNotFound()
+
+    hls_output_dir = os.path.join(Path(__file__).resolve().parent.parent, 'incidents')
+    hls_output_dir = os.path.join(hls_output_dir, f'incident_{pk}') 
+    
+    if not os.path.exists(hls_output_dir):
+        return HttpResponseNotFound()
+
+    playlist_path = os.path.join(hls_output_dir, filename)
+    if not os.path.exists(playlist_path):
+        return HttpResponseNotFound("Файл плейлиста не найден.")
+
+    with open(playlist_path, 'rb') as playlist_file:
+        response = HttpResponse(playlist_file.read(), content_type='application/vnd.apple.mpegurl')
+        return response
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('incidents/<int:pk>/<str:filename>', get_incident_view, name='get_incident'),
 ]
+
