7f350aee46e8723db8b33a30330c3720bed7c1e4
Kostenkov Vladislav <kostenkov.va@dvfu.ru>
2024-10-30 12:13:56 +1000

    added custom admin to Stream

--- camera_manager/admin.py
+++ camera_manager/admin.py
@@ -7,6 +7,7 @@ from .models import (
     CameraToGroup,
     Stream,
 )
+from back.settings import K8S_ADDRESS
 
 
 @admin.register(Camera)
@@ -17,12 +18,24 @@ class CameraAdmin(admin.ModelAdmin):
 class LocationsAdmin(admin.ModelAdmin):
     list_display = ('id', 'location')
 
+@admin.register(Stream)
+class StreamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'k8s_pod_name', 'k8s_pod_port', 'status', 'created_at', 'camera', 'stream_url')
+
+    def stream_url(self, obj):
+        if obj.k8s_pod_name and obj.k8s_pod_port:
+            return f"http://{K8S_ADDRESS}:{obj.k8s_pod_port}/convert_to_hls/streams/{obj.camera.id}/stream.m3u8"
+        return None
+
+    stream_url.short_description = 'Stream URL'
+
 admin.site.unregister(Camera)
 admin.site.register(Camera, CameraAdmin)
+admin.site.unregister(Stream)
+admin.site.register(Stream, StreamAdmin)
 admin.site.unregister(Location)
 admin.site.register(Location, LocationsAdmin)
 admin.site.register(GroupType)
 admin.site.register(CameraGroup)
 admin.site.register(CameraToGroup)
-admin.site.register(Stream)
 
